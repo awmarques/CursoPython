994b8370876edd3ca2ec9527b2f6bb384c0d422e
awmarques <139471143+awmarques@users.noreply.github.com>
2023-09-20 18:04:19 -0300

    exercicios

---
+++ Aulas/aula06a.py
@@ -0,0 +1,8 @@
+numero1 = int(input('Digite o primeiro número:'))
+numero2 = int(input('Digite o segundo número:'))
+soma = numero1+numero2
+print('Soma:{}'.format(soma))
+
+# comentário
+# print('A soma entre o número: {} '.format(numero1) + ' e o número: {} '.format(numero2) + ' é: {}'.format(soma))
+print('A soma entre o número: {} e o número: {} é igual a: {}!'.format(numero1, numero2, soma))
---
+++ Aulas/aula06b.py
@@ -0,0 +1,15 @@
+n = float(input('Digite um valor:'))
+print(n)
+
+n = str(input('Digite um valor:'))
+print(n)
+
+n = bool(input('Digite um valor:'))
+print(n)
+
+n= input('Digite algo:')
+print (n.isnumeric())
+
+n= input('Digite oi:')
+print (n.isalpha())
+
