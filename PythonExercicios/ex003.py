5ac6fedc091e103bfc2c5d533692292ef1f93060
awmarques <139471143+awmarques@users.noreply.github.com>
2023-09-22 13:39:25 -0300

    ex004

--- PythonExercicios/ex003.py
+++ PythonExercicios/ex003.py
@@ -1,4 +1,4 @@
 numero1 = int(input('Digite o primeiro número:'))
 numero2 = int(input('Digite o segundo número:'))
 soma = numero1+numero2
-print('Soma:{}'.format(soma))
+print('Soma entre {} e {} é: {}!'.format(numero1, numero2, soma))
