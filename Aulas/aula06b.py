ef825ac5731f71a485d11d7fc7c72f82ac7800f0
awmarques <139471143+awmarques@users.noreply.github.com>
2023-09-22 11:04:59 -0300

    Update aula06b.py

--- Aulas/aula06b.py
+++ Aulas/aula06b.py
@@ -7,9 +7,8 @@ print(n)
 n = bool(input('Digite um valor:'))
 print(n)
 
-n= input('Digite algo:')
-print (n.isnumeric())
+n = input('Digite algo:')
+print(n.isnumeric())
 
-n= input('Digite oi:')
-print (n.isalpha())
-
+n = input('Digite oi:')
+print(n.isalpha())
